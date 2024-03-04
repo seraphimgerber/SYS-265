7085f047e3a15ebc9e6ef1e2e44b428b2c797739
seraphimgerber <seraphim.gerber@mymail.champlain.edu>
2024-03-04 22:40:47 +0000

    did stuff

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,11 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE!"
+echo -n "Enter username: "
+read user
+useradd -m -d /home/$user -s /bin/bash $user
+mkdir /home/$user/.ssh
+cp SYS-265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+chmod 700 /home/$user/.ssh
+chmod 600 /home/$user/.ssh/authorized_keys
+chown -R $user:$user /home/$user/.ssh
